b639e06ee01b635be313f0a29e73f177953b6385
olavkal <olavkal@stud.ntnu.no>
2013-03-02 16:54:49 +0100

    en del på sm_macro
    fikset en del på sm_makroen og la til en metode
    i queue som jeg trengte

--- heis_code/main.c
+++ heis_code/main.c
@@ -11,31 +11,31 @@
 static int queues[N_QUEUES][N_FLOORS] = {{0}};
 
 int main()
-{ 
+{
 	//initialize states
 	int state = STATE_UNDEF;
 
     // Initialize hardware
-    if (!elev_init()) 
+    if (!elev_init())
     {
         printf(__FILE__ ": Unable to initialize elevator hardware\n");
         return 1;
     }
 
-    if (!ui_init()) 
+    if (!ui_init())
     {
         printf(__FILE__ ": Unable to initialize elevator user interface\n");
         return 1;
     }
 
     printf("Activate obstruction and press the STOP button to stop elevator and exit program.\n");
-    
-    //Move elevator towards next bottom floor 
+
+    //Move elevator towards next bottom floor
     if (elev_get_floor_sensor_signal() < 0)
     {
         elev_set_speed(-100);
     }
-	
+
     while (1) {
 
         //get button signal
@@ -44,61 +44,65 @@ int main()
         // Trenger en metode for å sjekke knapp input og legge det i kø array
         switch(state)
         {
-            
+
             case STATE_IDLE:
+            	elev_set_speed(0);
 				state = sm_idle(queues);
 				break;
             //ikke obst.
 
             //bestilling
-              
-            
+
+
             case STATE_UP:
-                // Finish up que itterativt so after floor 1 it only cares about 2-4 etc..
-                // IF order on floor above current flore(function that read) and elevator in motion upwards
+                // Finish up queue itterativt so after floor 1 it only cares about 2-4 etc..
+                // IF order on floor above current floor(function that read) and elevator in motion upwards
                 // when finished go to down after que_down check else go to idle
+                elev_set_speed(300);
 				state = sm_up(queues);
 				break;
-            
+
             case STATE_DOWN:
-                // Finish up que decitterativt so after floor 4 it only cares about 3-1 etc..
-                // IF order on floor above current flore(function that read) and elevator in motion upwards
+                // Finish up queue decitterativt so after floor 4 it only cares about 3-1 etc..
+                // IF order on floor above current floor(function that read) and elevator in motion upwards
                 // when finished go to up after que_down check else go to idle
+                elev_set_speed(-300);
 				state = sm_down(queues);
-            
+
             case STATE_DOOR_OPEN:
                 elev_set_speed(0);
                 while(elev_get_obstruction_signal() != 0){
-                        state = STATE_DOOR_OPEN;    
+                        state = STATE_DOOR_OPEN;
                 }
 				state = STATE_IDLE;
 				break;
                 //egen macro?
-                //sm_obstruction();    
-                
+                //sm_obstruction();
+
                 //light door pannel
-                
+
                 //usleep(3000);
-                //queue_delete que element 
-                //slett element i listen          
-            
+                //queue_delete que element
+                //slett element i listen
+
             case STATE_STOP:
                 elev_set_speed(0);
                 //kanskje gøre denne mer modulær type
 				state = sm_stop(queues);
 				break;
-            
+
             //case EMERGENCY:
 
             case STATE_UNDEF:
+            	//If the elevator do not are on a floor, move until it gets to the floor underneath
 				if (elev_get_floor_sensor_signal() < 0)
 				{
 					elev_set_speed(-100);
 					state = STATE_UNDEF;
 				}
-            
+
 				else if(elev_get_floor_sensor_signal() > 0){
-					state = STATE_IDLE;            
+					state = STATE_IDLE;
 				}
 				break;
                 //break;//crash it!
--- heis_code/sm_macro.c
+++ heis_code/sm_macro.c
@@ -7,23 +7,26 @@
 current_state_t sm_up(int queues[N_QUEUES][N_FLOORS]){
     int floor;
 	if(elev_get_stop_signal()) {
-        return STATE_STOP;    
+        return STATE_STOP;
     }
-	else if(queue_is_empty(queues,QUEUE_UP)){
-        return STATE_IDLE;    
+	else if(queue_is_empty(queues,QUEUE_UP) && !queue_check_relevant_command(get_floor_sensor_signal(), false)){
+        return STATE_IDLE;
     }
-    for(floor=0;floor<N_FLOORS;floor++){
-        //denne logikken fungerer kanskje ikke..
-		if(elev_get_floor_sensor_signal() == (queues[QUEUE_UP][floor] || queues[QUEUE_COMMAND][floor])){
-        //Open door
-            return STATE_DOOR_OPEN;
-        }
-    }
+    else {
+		for(floor=0;floor<N_FLOORS;floor++){
+			//denne logikken fungerer kanskje ikke..
+			if(elev_get_floor_sensor_signal() == (queues[QUEUE_UP][floor] || queues[QUEUE_COMMAND][floor])){
+			//Open door
+				return STATE_DOOR_OPEN;
+			}
+		}
+	}
+	//!!!!!!!Dette gjøres i maiN!!!!!!!
 	//Dette gikk ikke!
 	/*
 	else {
     elev_set_speed(300);
-        return STATE_UP;    
+        return STATE_UP;
     }
 	*/
 
@@ -32,21 +35,23 @@ current_state_t sm_up(int queues[N_QUEUES][N_FLOORS]){
 current_state_t sm_down(int queues[N_QUEUES][N_FLOORS]){
 	int floor;
     if(elev_get_stop_signal()) {
-        return STATE_STOP;    
+        return STATE_STOP;
     }
-	else if(queue_is_empty(queues,QUEUE_DOWN)){
-        return STATE_IDLE;    
+	else if(queue_is_empty(queues,QUEUE_DOWN) && !queue_check_relevant_command(get_floor_sensor_signal(), false)){
+        return STATE_IDLE;
     }
-    for(floor=3;floor>=0;floor--){
-		if(elev_get_floor_sensor_signal() == (queues[QUEUE_DOWN][floor] || queues[QUEUE_COMMAND][floor])){
-        //Open door
-            return STATE_DOOR_OPEN;
-        }
-    } 
-	
+    else {
+	    for(floor=3;floor>=0;floor--){
+			if(elev_get_floor_sensor_signal() == (queues[QUEUE_DOWN][floor] || queues[QUEUE_COMMAND][floor])){
+	        //Open door
+	            return STATE_DOOR_OPEN;
+	        }
+	    }
+	}
+
 	/*else {
     elev_set_speed(-300);
-        return STATE_DOWN;    
+        return STATE_DOWN;
     }
 	*/
 }
@@ -55,15 +60,15 @@ int sm_idle(int queues[N_QUEUES][N_FLOORS]) {
 	int floor;
     elev_set_speed(0);
 	if(elev_get_floor_sensor_signal() < 0){
-        return STATE_UNDEF;     
-    }    
-       //ser etter elementer i respektiv kø 
+        return STATE_UNDEF;
+    }
+       //ser etter elementer i respektiv kø
     if(queue_check_queues(queues)==0){
-    
+
         for(floor=0;floor<N_FLOORS;floor++){
 			if(queues[QUEUE_COMMAND][floor] == 1){
 
-                //spesialtilfelle må enders i state!       
+                //spesialtilfelle må enders i state!
                 if(elev_get_floor_sensor_signal() == floor || elev_get_floor_sensor_signal()<floor){
                 //Dette er bare for å åpne dør, via upstate
                     return STATE_UP;
@@ -71,7 +76,7 @@ int sm_idle(int queues[N_QUEUES][N_FLOORS]) {
                 else {
                     return STATE_DOWN;
                 }
-            }                
+            }
         }
     }
 	//Fryktelig forenklet utgave
@@ -82,12 +87,12 @@ int sm_idle(int queues[N_QUEUES][N_FLOORS]) {
 
     //2=nedkø
     else if(queue_check_queues(queues) == 2) {
-        return STATE_DOWN;    
+        return STATE_DOWN;
     }
 }
 
 current_state_t sm_stop(int queues[N_QUEUES][N_FLOORS]) {
-    elev_set_speed(0);    
+    elev_set_speed(0);
     queue_clear(queues);
     //evt ta høyde for obstruction
     return STATE_IDLE;
